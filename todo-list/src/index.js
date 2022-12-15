72a5689f86c574891aafbc5ebb5f7299a0c712f2
Chere Lemma <chere.lemma@aastu.edu.et>
2022-12-15 19:03:02 +0300

    Update index.js

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -3,41 +3,39 @@ import './style.css';
 
 const todoList = [
   [{
-    description:'Projects',
-    completed : true,
+    description: 'Projects',
+    completed: true,
     index: 1,
 }],
   [{
-    description:'Code review',
-    completed : true,
+    description: 'Code review',
+    completed: true,
     index: 2,
   }],
   [{
-    description:'Study lesson',
-    completed : false,
+    description: 'Study lesson',
+    completed: false,
     index: 3,
   }],
   [{
-    description:'Standup Meets',
-    completed : false,
+    description: 'Standup Meets',
+    completed: false,
     index: 3,
-  }]
+  }],
 ];
-
 const todoItem = document.querySelector('.todo-list')
 const myTodoList = () => {
-       // Lodash, now imported by this script
-    for (let i = 0; i< todoList.length; i++) {
-      const divElement = document.createElement('div');
-      divElement.classList.add('list-item')
-      const checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
-      divElement.appendChild(checkbox);
-      const label = document.createElement('label')
-      label.innerText = todoList[i][0].description
-      divElement.appendChild(label);
-      todoItem.appendChild(divElement);
-   }
+  // Lodash, now imported by this script
+  for (let i = 0; i< todoList.length; i++) {
+    const divElement = document.createElement('div');
+    divElement.classList.add('list-item')
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    divElement.appendChild(checkbox);
+    const label = document.createElement('label')
+    label.innerText = todoList[i][0].description
+    divElement.appendChild(label);
+    todoItem.appendChild(divElement);
   }
-  
-  myTodoList();
+}
+myTodoList();
