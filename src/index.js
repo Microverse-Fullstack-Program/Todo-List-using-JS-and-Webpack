f6861c8e695f4f4880024c29199da59279a5dd1f
cherelemma <73167960+cherelemma@users.noreply.github.com>
2022-12-15 22:34:53 +0300

    Update to-do list array element

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 import './style.css';
 
 const todoList = [
-  [{
+  {
     description: 'Projects',
     completed: true,
     index: 1,
-  }],
-  [{
+  },
+  {
     description: 'Code review',
     completed: true,
     index: 2,
-  }],
-  [{
+  },
+  {
     description: 'Study lesson',
     completed: false,
     index: 3,
-  }],
-  [{
+  },
+  {
     description: 'Standup Meets',
     completed: false,
     index: 3,
-  }],
+  },
 ];
 
 const todoItem = document.querySelector('.todo-list');
 const myTodoList = () => {
   // Lodash, now imported by this script
-  for (let i = 0; i < todoList.length; i++) {
+  todoList.map((listElement) =>  {
     const divElement = document.createElement('div');
     divElement.classList.add('list-item');
     const checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
     divElement.appendChild(checkbox);
     const label = document.createElement('label');
-    label.innerText = todoList[i][0].description;
+    label.innerText = listElement.description;
     divElement.appendChild(label);
     todoItem.appendChild(divElement);
-  }
+  })
 };
 myTodoList();
