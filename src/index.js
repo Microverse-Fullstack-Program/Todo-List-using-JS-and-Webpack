2647f08eb8f7f674eaf5180c814500b9ae1d6648
cherelemma <73167960+cherelemma@users.noreply.github.com>
2022-12-24 12:02:52 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import displayTodoList from './modules/displayTodo.js';
 import editTodoTask from './modules/editTask.js';
 import removeAllCheckedItem from './modules/removeAllSelected.js';
 import removeTask from './modules/removeTask.js';
-import readLocalStorage from './modules/storage.js';
-import resetIndex from './modules/resetIndex';
 
 const form = document.querySelector('.form');
 const todoInput = document.querySelector('.todo-item-input');
@@ -23,49 +21,6 @@ form.addEventListener('submit', (event) => {
   }
 });
 
-// eventListener for check/uncheck todo tasks
-document.addEventListener('change', (e) => {
-  if (e.target.className === 'editing-input') {
-    const updatedValue = e.target.value;
-    let editIndex = e.target.parentElement.parentElement.parentElement.dataset;
-    editIndex = parseInt(editIndex.index)
-    const updateTodoList = readLocalStorage();
-    updateTodoList.forEach((todoTask) => {
-      if (todoTask.index === editIndex && updatedValue) {
-        todoTask.description = updatedValue;
-      }
-    });
-    localStorage.setItem('todo_List', JSON.stringify(updateTodoList));
-    window.location.reload();
-  }
-  else if (e.target.className === 'checkBtn') {
-    const targetElement = e.target;
-    const todoTaskTitle = targetElement.nextElementSibling.childNodes[0];
-    const todoTaskContainer = targetElement.parentElement.parentElement;
-    const { index } = todoTaskContainer.dataset;
-    const todoListList = readLocalStorage();
-    if (targetElement.checked === true) {
-      todoTaskTitle.style.textDecoration = 'line-through';
-  
-      todoListList.filter((todoTask) => {
-        if (todoTask.index === parseInt(index)) {
-          todoTask.completed = true;
-        }
-        return false;
-      });
-    } else {
-      todoTaskTitle.style.textDecoration = 'none';
-      todoListList.filter((todoTask) => {
-        if (todoTask.index === index) {
-          todoTask.completed = false;
-        }
-        return false;
-      });
-    }
-    localStorage.setItem('todo_List', JSON.stringify(todoListList));
-  }
-});
-
 // eventListener for option and remove button
 document.addEventListener('click', (e) => {
   if (e.target.className === 'fa-solid fa-ellipsis-vertical') {
@@ -74,14 +29,10 @@ document.addEventListener('click', (e) => {
 
     const delBtn = e.target.parentElement.lastChild;
     delBtn.classList.add('show-trashBtn');
-    editTodoTask(e);
+    editTodoTask(e, delBtn, optionBtn);
   } else if (e.target.className === 'fa-solid fa-trash-clock fa-trash show-trashBtn') {
     const { index } = e.target.parentElement.parentElement.dataset;
-    removeTask(index);
-    resetIndex();
-    window.location.reload();
-  } else if (e.target.className === 'fa-solid fa-rotate') {
-    localStorage.removeItem('todo_List')
+    todoList = removeTask(index);
     window.location.reload();
   }
 });
@@ -90,8 +41,6 @@ document.addEventListener('click', (e) => {
 clearBtn.addEventListener('click', (e) => {
   e.preventDefault();
   removeAllCheckedItem();
-  resetIndex();
-  window.location.reload();
 });
 
 window.addEventListener('load', () => {
